8e35b3e518e2fc634eb2bc2d4d4170605aa031ca
ngotan159 <ngotan159@gmail.com>
2024-10-31 09:17:53 +0700

    thongbao

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,24 +3,6 @@
 <div class="message_mini">
     @yield('content')
     <div class="container-fluid hehe">
-         {{-- Hiện thông báo cập nhật user --}}
-    <div class="notification-container">
-        @if (session('success'))
-            <div class="alert alert-success" role="alert">
-                {{ session('success') }}
-            </div>
-        @endif
-    
-        @if ($errors->any())
-            <div class="alert alert-danger">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
-    </div>
         <div class="row">
             <!-- Left Sidebar -->
             <section class="col-1 sidebar">
